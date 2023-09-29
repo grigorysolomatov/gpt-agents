cea9bb2d0fe79b6b27f923eeac8ecb06ae3dc320
grigorysolomatov <30951147+grigorysolomatov@users.noreply.github.com>
2023-09-29 09:45:30 +0300

    Add error handling for shell command

--- functions.py
+++ functions.py
@@ -22,4 +22,13 @@ class RunShellCommand:
     @staticmethod
     def run(args):
         args = json.loads(args)
-        return subprocess.check_output(args["shell_command"], shell=True).decode()
+        try:
+            return subprocess.run(args["shell_command"], capture_output=True, text=True, check=True, shell=True).stdout
+        except subprocess.CalledProcessError as e:
+            return e.stderr
+        #try:            
+        #    return subprocess.check_output(args["shell_command"], shell=True).decode()
+        #except subprocess.CalledProcessError as e:
+        #    return str(e)
+        #except Exception as e:
+        #    return str(e)
